6f5da0821ab578e854d89795bbcd7a293df67472
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-08-10 19:04:02 +0530

    Added one leetcode array problem

--- Basic/index.js
+++ Basic/index.js
@@ -7,47 +7,46 @@
 // let and const is a local scoped variable declaration and var is global scoped variable decalaration.
 
 // const checkerFunction = ()=> {
-    
-    // console.log(variable)
 
-    // const variable = "const" // reference error cannot access variable before initialisation
+// console.log(variable)
 
-    // let variable = "let"  // reference error cannot access variable before initialisation
+// const variable = "const" // reference error cannot access variable before initialisation
 
-    // var variable = "var"  // undefined
+// let variable = "let"  // reference error cannot access variable before initialisation
 
-    // __________const section___________
-    // const variable = "first const"
+// var variable = "var"  // undefined
 
-    // if(true) {
-    //     const variable = "second const"
-    //     console.log(variable) // second const
-        
-    // }
-    // console.log(variable)  // first const
-     // __________const section___________
+// __________const section___________
+// const variable = "first const"
 
+// if(true) {
+//     const variable = "second const"
+//     console.log(variable) // second const
 
-    // __________let section___________
-    // let variable = "first let"
+// }
+// console.log(variable)  // first const
+// __________const section___________
 
-    // if(true) {
-    //     let variable = "second let"
-    //     // console.log(variable) // second let
-        
-    // }
-    // console.log(variable) // first let
-    // __________let section___________
+// __________let section___________
+// let variable = "first let"
 
-    // __________var section___________
-    // var variable = "fist var"
-    // if(true) {
-    //     var variable = "second var"
-    //     console.log(variable)  // second var
-       
-    // }
-    //  console.log(variable)  // second var
-    // __________var section___________
+// if(true) {
+//     let variable = "second let"
+//     // console.log(variable) // second let
+
+// }
+// console.log(variable) // first let
+// __________let section___________
+
+// __________var section___________
+// var variable = "fist var"
+// if(true) {
+//     var variable = "second var"
+//     console.log(variable)  // second var
+
+// }
+//  console.log(variable)  // second var
+// __________var section___________
 
 // }
 
@@ -57,14 +56,13 @@
 
 // _____________________________________________________________
 // 3. How does hoisting work in JavaScript?
-// a. What is hoisting? 
+// a. What is hoisting?
 // ans: Hoisting means if try to access the variable of object before intialisation we get different warnings or errors
 // if we use let or const for declaring that variable we will reference error cannot access variable before intialisation
 // but if we use var to declare that variable we will get undefined.
 // Everything inside javascript is happens in a execution context.
 // firstly javascript look for is there any variable or function that is declared? if yes then the javascript firstly take that to a memory phase. now if we initalise a variable with a value, it won't assigned that first time, so when we console log that it will gives us reference error if we used let or const and undefined in var.
 // _____________________________________________________________
-
 
 // ________________________________________________________________
 // 4. Describe the concept of closures.
@@ -80,9 +78,8 @@
 
 // const userDetail = getUserDetailOuter("Kevin");
 // userDetail(); // This will log: From inner function Kevin
-// This is what that meant by closures that, the inner function that a function inside a function can access the values of the outer function. 
+// This is what that meant by closures that, the inner function that a function inside a function can access the values of the outer function.
 // ________________________________________________________________
-
 
 // 5. What are all the looping structures in JavaScript ?
 // ans: 1. While-loop
@@ -97,9 +94,11 @@
 // 6. How can the style/class of an element be changed?
 // ans: Let say that we have a div and we need to change the class and style of that div.
 
-{/* <div class="div">
+{
+     /* <div class="div">
 Hello
-</div> */}
+</div> */
+}
 
 // const result = document.getElementById('div').style.fontSize = "6rem"
 // From the above we can change the font size of that div
@@ -109,7 +108,6 @@ Hello
 // const result = document.getElementById('div').className = "new-div"
 
 // ________________________________________________________________
-
 
 // 7. What is called Variable typing in JavaScript ?
 // ans: Lets say we have created a variable in javascript and named it to
@@ -122,11 +120,11 @@ Hello
 
 // console.log(typeof value);
 
-// we can change the type of the variable in javascript. And we won't get any errors while changing the type.  
+// we can change the type of the variable in javascript. And we won't get any errors while changing the type.
 
 // ________________________________________________________________
 
-// 8. Write a JavaScript program to find the maximum number in an array. 
+// 8. Write a JavaScript program to find the maximum number in an array.
 // ans: const arr = [1, 2, 3, 4, 5, 6, 10]
 // let newValue = Math.max(...arr);
 // console.log(typeof newValue)
@@ -142,14 +140,14 @@ Hello
 // let arr3 = [...arr1, ...arr2]
 // console.log(arr3) // we are getting whole array ie, [1, 2, 3, 4, 5, 6, 7, 8]
 
-// Rest Operator: When we are getting more parameters. we can use spread operator. spread operator is too looking like rest operator. 
- //
+// Rest Operator: When we are getting more parameters. we can use spread operator. spread operator is too looking like rest operator.
+//
 
 //  const getUserDetails = (...details)=> {
 //     console.log(details)
 //     let userName = [details[0]]
 //     console.log(userName)
-//  }  
+//  }
 
 //  getUserDetails("kevin", 22, "Software Engineer", '6LPA')
 
@@ -159,8 +157,8 @@ Hello
 // ________________________________________________________________
 
 // 10. Explain what is callback function is and explain with an example?
-// ans: A callback function is a function that is passsed to a function's argument. 
-// eg is 
+// ans: A callback function is a function that is passsed to a function's argument.
+// eg is
 
 // const handleResult = (number, showResult)=> {
 //     console.log("started")
@@ -174,5 +172,39 @@ Hello
 // }
 // const getResult = handleResult(2, showResult)
 // console.log(getResult)
+
+// ________________________________________________________________
+// 11. Question is we are passing an array of number, we need to add the number and return like, if we are passing the input as [1, 2, 3, 4, 5] we need the output of [1, 3, 6, 10]
+// let runningSum = function (nums) {
+//      let result = [];
+
+//      let cumulativeSum = 0;
+
+//      for (let num of nums) {
+//           cumulativeSum += num;
+
+//           result.push(cumulativeSum);
+//      }
+
+//      return result;
+// };
+
+// const output = runningSum([1, 2, 3, 4]);
+
+// ________________________________________________________________
+
+// 12. The question is return true if the words first letter in the words array and value in s variable are same otherwise return false
+
+// const isAcronym = function (words, s) {
+//      const value = words.map((item) => {
+//           return item.charAt(0);
+//      });
+//      const result = value.toString().replace(/,/g, "");
+//      let finalRes;
+//      result === s ? (finalRes = true) : (finalRes = false);
+//      return finalRes;
+// };
+// isAcronym(["alice", "bob", "charlie"], "abc");
+// console.log(isAcronym);
 
 // ________________________________________________________________
