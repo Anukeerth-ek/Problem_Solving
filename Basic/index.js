a9b6b271c7db1d220ef82e375b4b63fd72bbe9fe
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-10-20 23:52:24 +0530

    Added one easy: question

--- Basic/index.js
+++ Basic/index.js
@@ -532,3 +532,20 @@ Hello
 // };
 // vowelStrings(["hey", "aeo", "mu", "ooo", "artro"], 1, 4);
 // ________________________________________________________________
+
+// ________________________________________________________________
+// 25. Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in 
+// nums which are not equal to val.
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size 
+// of nums.
+// Return k.
+
+// ans:
+var removeElement = function(nums, val) {
+  
+ };
+
+ const result = removeElement([3,2,2,3], 3)
+ 
+// ________________________________________________________________
