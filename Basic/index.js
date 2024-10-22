21a9120a9099f9208441ba3882a5c448dfaf9b9c
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-10-22 08:48:35 +0530

    Added Array Easy question:

--- Basic/index.js
+++ Basic/index.js
@@ -534,18 +534,28 @@ Hello
 // ________________________________________________________________
 
 // ________________________________________________________________
-// 25. Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in 
-// nums which are not equal to val.
+// 25. Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the
+// number of elements in nums which are not equal to val.
 // Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
-// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size 
-// of nums.
-// Return k.
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important
+// as well as the size of nums. Return k.
 
 // ans:
-var removeElement = function(nums, val) {
-  
- };
+const removeElement = (nums, val) => {
+     let index = 0;
 
- const result = removeElement([3,2,2,3], 3)
- 
+     for (let i = 0; i < nums.length; i++) {
+          if (nums[i] !== val) {
+               nums[index] = nums[i]; // Shift non-val elements to the left
+               index++;
+          }
+     }
+
+     return index;
+};
+
+console.log(removeElement([3, 2, 2, 3], 3));
+
+//  Expected output = [ 2, 2, ]
+
 // ________________________________________________________________
