3f676f2f2b4aafc363998c755be3b77c8116e9d7
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-10-27 11:31:27 +0530

    Added codechef array problem:

---
+++ CodeChefProblems/index.js
@@ -0,0 +1,30 @@
+// ________________________________________________________________
+// 1. Search an element in an array
+
+// You are given an array A of size N and an element X. Your task is to determine whether the array A contains the element X or not.
+
+// Input Format:
+// The first line contains two space-separated integers N and X:
+// N: the size of the array.
+// X: the element to be searched in the array.
+// The second line contains N space-separated integers, which are the elements of array A.
+// Output Format:
+// Output "YES" if the element X is present in the array A.
+// Otherwise, output "NO".
+// ans:
+// const searchNumber = (numLength, searchNumber, ...arr)=> {
+//     let flag = false;
+//     for(let i = 0; i < numLength; i++) {
+//         if(arr[i] === searchNumber) {
+//             flag = true
+//             break;
+//         }
+//         else flag = false
+        
+//     }
+//     return flag
+// }
+
+// console.log(searchNumber(5, 13,
+// 7, 3, 5, 2, 1))
+// ________________________________________________________________
