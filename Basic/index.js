991e56fb2338d20887df18804f708b0ed2cecfbc
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-12-07 00:20:40 +0530

    feat(new problem): Added new problem

--- Basic/index.js
+++ Basic/index.js
@@ -745,9 +745,16 @@ Hello
 // Two Sum Problem: Find two numbers in an array that add up to a target sum.
 // ans:
 
-const returnTwoSum = (arr, target)=> {
-   // console.log(arr)
-   
-}
+// function twoSum(nums, target) {
+//    for (let i = 0; i < nums.length; i++) {
+//        for (let j = i + 1; j < nums.length; j++) {
+//            if (nums[i] + nums[j] === target) {
+//                return [i, j];
+//            }
+//        }
+//    }
+//    return [];
+// }
 
-console.log(returnTwoSum([2, 7, 11, 15], 9))
+// console.log(twoSum([2, 7, 11, 15], 9));
+// ________________________________________________________________
