c12f82d317fc0a5c8fd15bd9fb6b2572aba0e3a6
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-12-31 08:35:10 +0530

    added second approch setup

--- Basic/index.js
+++ Basic/index.js
@@ -1031,14 +1031,19 @@ Hello
 // ________________________________________________________________
 // 48. Program to find Reverse of a string without using built-in method ?
 // ans:
+
 // const getReversedString = (string) => {
+
+// ___________This is a First approach____________
 //      let newArr = [];
 //      for (let letter = string.length - 1; letter >= 0; letter--) {
 //           newArr.push(string[letter]);
 //      }
 //      return newArr.toString().replaceAll(",", "");
+
+// ___________This is a Second approach____________
+
 // };
 
 // console.log(getReversedString("Hello"));
-
 // ________________________________________________________________
