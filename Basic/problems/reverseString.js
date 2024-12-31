1d026d370e8a211cd1bc9b1669ee6de471cb273f
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-12-31 11:14:15 +0530

    added test cases for the problems

---
+++ Basic/problems/reverseString.js
@@ -0,0 +1,17 @@
+// // Program to find Reverse of a string without using built-in method ?
+
+// const getReversedString = (string) => {
+//      // ___________This is a First approach____________
+//     //  let newArr = [];
+//     //  for (let letter = string.length - 1; letter >= 0; letter--) {
+//     //       newArr.push(string[letter]);
+//     //  }
+//     //  return newArr.toString().replaceAll(",", "");
+
+//      // ___________This is a Second approach____________
+//    return string.split('').reverse('').join('');
+  
+// };
+
+// console.log(getReversedString("Hello"));
+// export { getReversedString };
---
+++ Basic/tests/reverseString.test.js
@@ -0,0 +1,20 @@
+// import { getReversedString } from "../problems/reverseString.js";
+
+// // Basic Tests
+// test('reverse a string', ()=> {
+//     expect(getReversedString("Hello")).toBe("olleH")
+// })
+
+// test('reverse single string', ()=> {
+//     expect(getReversedString('a')).toBe('a')
+// })
+
+// // Edge Case
+
+// test('reverse an empty string', ()=> {
+//     expect(getReversedString('')).toBe('')
+// })
+
+// test('spaced string test', ()=> {
+//     expect(getReversedString('a b c')).toBe('c b a')
+// })
