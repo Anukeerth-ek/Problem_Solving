fcbe29a9c8cdd4ac425594847aaf4e2d0c4131dc
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-01-01 23:56:28 +0530

    added problem approach

---
+++ Basic/problems/maxConsecutiveNumber.js
@@ -0,0 +1,15 @@
+// Problem: Find the max count of consecutive 1’s in an array ?
+
+// So our task is to return the maximum consecutive 1's in an array.
+
+const getMaxConsecutiveOnes = (arr)=> {
+    let newArr = []
+    for(let i = 0; i < arr.length; i++) {
+        
+    }
+}
+
+console.log(getMaxConsecutiveOnes([1, 1, 2, 3, 4, 1, 1, 1]))
+
+// node maxConsecutiveNumber
+
