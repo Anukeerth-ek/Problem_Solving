c0854de9cd5cf1ae82bc835fe5d6680bd451823c
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-01-02 23:48:32 +0530

    added maxconsecutive problem

--- Basic/problems/maxConsecutiveNumber.js
+++ Basic/problems/maxConsecutiveNumber.js
@@ -1,25 +1,25 @@
-// Problem: Find the max count of consecutive 1’s in an array ?
+// // Problem: Find the max count of consecutive 1’s in an array ?
 
-// So our task is to return the maximum consecutive 1's in an array.
+// // So our task is to return the maximum consecutive 1's in an array.
 
-const getMaxConsecutiveOnes = (arr) => {
-     let newArr = [];
-     let arrayLength ;
-     for (let i = 0; i < arr.length; i++) {
-          for (let j = i; j <=i; j++) {
-               if (arr[i] === 1) {
-                    newArr.push(arr[i]);
-                    arrayLength = newArr.length;
-               } else {
-                newArr = []
-                break;
-               }
-          }
-     }
-    return newArr.length
-};
+// const getMaxConsecutiveOnes = (arr) => {
+//      let newArr = [];
+//      let arrayLength ;
+//      for (let i = 0; i < arr.length; i++) {
+//           for (let j = i; j <=i; j++) {
+//                if (arr[i] === 1) {
+//                     newArr.push(arr[i]);
+//                     arrayLength = newArr.length;
+//                } else {
+//                 newArr = []
+//                 break;
+//                }
+//           }
+//      }
+//     return newArr.length
+// };
 
 
-// node maxConsecutiveNumber
+// // node maxConsecutiveNumber
 
-export {getMaxConsecutiveOnes}
+// export {getMaxConsecutiveOnes}
--- Basic/tests/maxConsecutiveNumber.test.js
+++ Basic/tests/maxConsecutiveNumber.test.js
@@ -1,56 +1,56 @@
-import { getMaxConsecutiveOnes } from "../problems/maxConsecutiveNumber";
+// import { getMaxConsecutiveOnes } from "../problems/maxConsecutiveNumber";
 
-describe("find max consecutive Ones", () => {
-     test("checking with 3 ones", () => {
-          expect(getMaxConsecutiveOnes([1, 1, 2, 3, 4, 1, 1, 1])).toBe(3);
-     });
+// describe("find max consecutive Ones", () => {
+//      test("checking with 3 ones", () => {
+//           expect(getMaxConsecutiveOnes([1, 1, 2, 3, 4, 1, 1, 1])).toBe(3);
+//      });
 
-     test("checking with 2 ones", () => {
-          expect(getMaxConsecutiveOnes([1, 2, 2, 3, 4, 5, 1, 1])).toBe(2);
-     });
-     test("checking with 4 ones", () => {
-          expect(getMaxConsecutiveOnes([1, 1, 2, 3, 1, 1, 1, 1])).toBe(4);
-     });
-     test("checking with full ones", () => {
-          expect(getMaxConsecutiveOnes([1, 1, 1, 1, 1, 1, 1, 1])).toBe(8);
-     });
-     test('Simple case', () => {
-        expect(getMaxConsecutiveOnes([1, 1, 0, 1, 1, 1])).toBe(3);
-      });
+//      test("checking with 2 ones", () => {
+//           expect(getMaxConsecutiveOnes([1, 2, 2, 3, 4, 5, 1, 1])).toBe(2);
+//      });
+//      test("checking with 4 ones", () => {
+//           expect(getMaxConsecutiveOnes([1, 1, 2, 3, 1, 1, 1, 1])).toBe(4);
+//      });
+//      test("checking with full ones", () => {
+//           expect(getMaxConsecutiveOnes([1, 1, 1, 1, 1, 1, 1, 1])).toBe(8);
+//      });
+//      test('Simple case', () => {
+//         expect(getMaxConsecutiveOnes([1, 1, 0, 1, 1, 1])).toBe(3);
+//       });
     
-      test('Single element (1)', () => {
-        expect(getMaxConsecutiveOnes([1])).toBe(1);
-      });
+//       test('Single element (1)', () => {
+//         expect(getMaxConsecutiveOnes([1])).toBe(1);
+//       });
     
-      test('Single element (0)', () => {
-        expect(getMaxConsecutiveOnes([0])).toBe(0);
-      });
+//       test('Single element (0)', () => {
+//         expect(getMaxConsecutiveOnes([0])).toBe(0);
+//       });
     
-      test('No 1s in the array', () => {
-        expect(getMaxConsecutiveOnes([0, 0, 0, 0])).toBe(0);
-      });
+//       test('No 1s in the array', () => {
+//         expect(getMaxConsecutiveOnes([0, 0, 0, 0])).toBe(0);
+//       });
     
-      test('All 1s in the array', () => {
-        expect(getMaxConsecutiveOnes([1, 1, 1, 1])).toBe(4);
-      });
+//       test('All 1s in the array', () => {
+//         expect(getMaxConsecutiveOnes([1, 1, 1, 1])).toBe(4);
+//       });
     
-      test('Mixed case with single 1s', () => {
-        expect(getMaxConsecutiveOnes([0, 1, 0, 1, 0])).toBe(1);
-      });
+//       test('Mixed case with single 1s', () => {
+//         expect(getMaxConsecutiveOnes([0, 1, 0, 1, 0])).toBe(1);
+//       });
     
-      test('Multiple streaks of 1s', () => {
-        expect(getMaxConsecutiveOnes([1, 1, 0, 1, 0, 1, 1, 1])).toBe(3);
-      });
+//       test('Multiple streaks of 1s', () => {
+//         expect(getMaxConsecutiveOnes([1, 1, 0, 1, 0, 1, 1, 1])).toBe(3);
+//       });
     
-      test('Empty array', () => {
-        expect(getMaxConsecutiveOnes([])).toBe(0);
-      });
+//       test('Empty array', () => {
+//         expect(getMaxConsecutiveOnes([])).toBe(0);
+//       });
     
-      test('Edge case with large input', () => {
-        expect(getMaxConsecutiveOnes([1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1])).toBe(4);
-      });
+//       test('Edge case with large input', () => {
+//         expect(getMaxConsecutiveOnes([1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1])).toBe(4);
+//       });
     
-      test('Alternating 1s and 0s', () => {
-        expect(getMaxConsecutiveOnes([1, 0, 1, 0, 1, 0, 1])).toBe(1);
-      });
-});
+//       test('Alternating 1s and 0s', () => {
+//         expect(getMaxConsecutiveOnes([1, 0, 1, 0, 1, 0, 1])).toBe(1);
+//       });
+// });
