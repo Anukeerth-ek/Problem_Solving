d88e6f5637bb890ca283bc177f85292ed05eeb1e
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-01-04 00:00:32 +0530

    passed another test

--- Basic/tests/factorials.test.js
+++ Basic/tests/factorials.test.js
@@ -4,4 +4,7 @@ describe('calculating factorials of a number', ()=> {
     test('checking with 2 digit number', ()=> {
         expect(calculateFactorial(10)).toBe(3628800)
     })
+    test('checking with large number', ()=> {
+        expect(calculateFactorial(12)).toBe(479001600)
+    })
 })
