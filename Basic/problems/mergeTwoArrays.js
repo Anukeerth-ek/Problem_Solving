3c06fe49c2d2fc5a58cbf46cc18efe98e8cb4f93
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-01-05 18:01:07 +0530

    cleared up the problem

--- Basic/problems/mergeTwoArrays.js
+++ Basic/problems/mergeTwoArrays.js
@@ -2,21 +2,11 @@
 
 const handleMergeSortedArray = (arr1, arr2) => {
      const mergeArray = [...arr1, ...arr2];
-    //  let newArr = [];
-    //  for (let i = 0; i < mergeArray.length; i++) {
-    //       for (let j = i + 1; j <= mergeArray.length; j++) {
-    //            if (mergeArray[i] > mergeArray[j]) {
-    //                 newArr.push(mergeArray[i]);
-    //            } else {
-    //                 continue
-    //            }
-    //       }
-    //  }
-    //  console.log("result", newArr);
-    const sortedArray = mergeArray.sort((first, second)=> first - second)
-    // console.log("sro", sortedArray)
-    return sortedArray;
+
+     const sortedArray = mergeArray.sort((first, second) => first - second);
+
+     return sortedArray;
 };
 console.log(handleMergeSortedArray([1, 5, 9], [2, 6, 10]));
 
-export {handleMergeSortedArray}
+export { handleMergeSortedArray };
