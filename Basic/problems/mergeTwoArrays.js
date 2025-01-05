fed73de687fae92385b6ba826feb012cd2419add
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-01-05 17:48:46 +0530

    added first approach

--- Basic/problems/mergeTwoArrays.js
+++ Basic/problems/mergeTwoArrays.js
@@ -1,10 +1,19 @@
-// Problem: Given 2 arrays that are sorted [0,3,4,31] and [4,6,30]. Merge them and sort [0,3,4,4,6,30,31] 
+// Problem: Given 2 arrays that are sorted [0,3,4,31] and [4,6,30]. Merge them and sort [0,3,4,4,6,30,31]
 
-const handleMergeSortedArray = (arr1, arr2)=> {
-    const mergeArray = [...arr1, ...arr2];
-    for(let i = mergeArray.length; i >= 0; i--) {
-            console.log(mergeArray[i])
-    }
-    console.log(mergeArray)
-}
-console.log(handleMergeSortedArray([0,3,4,31], [4,6,30]))
+const handleMergeSortedArray = (arr1, arr2) => {
+     const mergeArray = [...arr1, ...arr2];
+    //  let newArr = [];
+    //  for (let i = 0; i < mergeArray.length; i++) {
+    //       for (let j = i + 1; j <= mergeArray.length; j++) {
+    //            if (mergeArray[i] > mergeArray[j]) {
+    //                 newArr.push(mergeArray[i]);
+    //            } else {
+    //                 continue
+    //            }
+    //       }
+    //  }
+    //  console.log("result", newArr);
+    const sortedArray = mergeArray.sort((first, second)=> second - first)
+    console.log("sro", sortedArray)
+};
+console.log(handleMergeSortedArray([0, 3, 4, 31], [4, 6, 30]));
