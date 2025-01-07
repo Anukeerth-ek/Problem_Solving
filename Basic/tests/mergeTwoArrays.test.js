dfa84f696bb9f2a5ec8edda6ae7484fbce5d7473
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-01-07 23:49:07 +0530

    added new test cases

--- Basic/tests/mergeTwoArrays.test.js
+++ Basic/tests/mergeTwoArrays.test.js
@@ -4,4 +4,9 @@ describe("Merge and sort the array", ()=> {
     test('with sample test', ()=> {
         expect(handleMergeSortedArray([1, 5, 9], [2, 6, 10])).toEqual([1, 2, 5, 6, 9, 10])
     })
+
+    test('with some hard test cases', ()=> {
+        expect(handleMergeSortedArray([0, 0, 10, 100], [100, 10, 0, 0])).toEqual([0, 0, 0, 0, 10, 10, 100, 100])
+    })
+
 })
