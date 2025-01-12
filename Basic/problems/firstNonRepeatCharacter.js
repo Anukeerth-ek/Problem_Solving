8ca41390742b05edeabae3d3e682afc280612daf
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-01-12 22:22:23 +0530

    added new problem setup

---
+++ Basic/problems/firstNonRepeatCharacter.js
@@ -0,0 +1,16 @@
+// Problem: Find the first non-repeating character in a string.
+
+// const handleFindNonRepeatCharacter = (characterArr) => {
+//      const character = characterArr.split("");
+
+//      for (let i = 0; i < character.length; i++) {
+//           for (let j = 0; j < character.length; j++) {
+//                if()
+//           }
+//      }
+// };
+
+// console.log(handleFindNonRepeatCharacter("aabbcc")); // A
+// console.log(handleFindNonRepeatCharacter("leetcode")); // l
+// // console.log(handleFindNonRepeatCharacter("loveleetcode")); // v
+// // console.log(handleFindNonRepeatCharacter("aabb")); // 0
