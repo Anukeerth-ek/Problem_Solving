5d313eba5ccb7b2cfac893df23a7f11fcd6f8b19
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-03-04 13:45:36 +0530

    added new problem solution

--- DSA/Arrays_&_String/shuffle_String.js
+++ DSA/Arrays_&_String/shuffle_String.js
@@ -1,11 +1,15 @@
-// You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith 
+// You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith
 // position moves to indices[i] in the shuffled string.
 
 // Return the shuffled string.
 
+var restoreString = function (s, indices) {
+     const indicesController = [];
 
-var restoreString = function(s, indices) {
-
+     for (let i = 0; i < s.length; i++) {
+          indicesController[indices[i]] = s[i];
+     }
+     return indicesController.join("");
 };
 
-console.log(restoreString("codeleet", [4,5,6,7,0,2,1,3]))
+console.log(restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]));
