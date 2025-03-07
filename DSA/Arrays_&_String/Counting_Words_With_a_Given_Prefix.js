8b6db7769aa51aec8dfce0426a7c6e9db4c64fd2
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-03-07 11:37:15 +0530

    Added new problem support

---
+++ DSA/Arrays_&_String/Counting_Words_With_a_Given_Prefix.js
@@ -0,0 +1,14 @@
+// You are given an array of strings words and a string pref.
+
+// Return the number of strings in words that contain pref as a prefix.
+
+// A prefix of a string s is any leading contiguous substring of s
+
+// ans:
+
+var prefixCount = function(words, pref) {
+    
+    console.log("w", words, pref)
+};
+
+prefixCount(["pay","attention","practice","attend"], "at")
