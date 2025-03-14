27a9f668391cca50a1c97f3b0bbd62f62722e15a
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-03-15 00:31:57 +0530

    Added the correct proper solution

--- DSA/Arrays_&_String/1436.js
+++ DSA/Arrays_&_String/1436.js
@@ -1,19 +1,28 @@
-// You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the 
+// You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the
 // destination city, that is, the city without any path outgoing to another city.
 // It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
 
 // ans:
 
-var destCity = function(paths) {
+var destCity = function (paths) {
+     let destination = new Set();
+     for (let path of paths) {
+          destination.add(path[0]);
+     }
 
-    let destination;
-    for(let i = 0; i < paths.length; i++) {
-        const arrayLastIndex = paths[paths.length - 1];
-        destination = arrayLastIndex[arrayLastIndex.length - 1]
-    }
-    return destination;
+     for(let path of paths) {
+        if(!destination.has(path[1])){
+            return path[1]
+        }
+     }
 };
 
-console.log(destCity([["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]])) // Sao Paulo
+console.log(
+     destCity([
+          ["London", "New York"],
+          ["New York", "Lima"],
+          ["Lima", "Sao Paulo"],
+     ])
+); // Sao Paulo
 console.log(destCity([["B","C"],["D","B"],["C","A"]])) // A
 console.log(destCity([["A","Z"]])) // Z
