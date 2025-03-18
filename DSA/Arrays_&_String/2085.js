72242b23cbae433006903fe23bf2f3d057f34b8b
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-03-18 09:37:58 +0530

    Added new problem setup:

---
+++ DSA/Arrays_&_String/2085.js
@@ -0,0 +1,6 @@
+// Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.
+
+var countWords = function(words1, words2) {
+};
+
+console.log(countWords(["leetcode","is","amazing","as","is"], ["amazing","leetcode","is"]))
