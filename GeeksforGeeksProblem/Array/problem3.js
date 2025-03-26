cf3070543a61111d1fe6cb940669074694c6b3e3
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-03-26 09:09:14 +0530

    Added new problem setup:

---
+++ GeeksforGeeksProblem/Array/problem3.js
@@ -0,0 +1,21 @@
+// Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then 
+// return -1.
+
+// Note: The second largest element should not be equal to the largest element.
+
+// Examples:
+
+// Input: arr[] = [12, 35, 1, 10, 34, 1]
+// Output: 34
+// Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+class Solution {
+    // Function returns the second largest element
+    getSecondLargest(arr) {
+        
+    }
+}
+
+const solution = new Solution()
+
+console.log(solution.getSecondLargest([12, 35, 1, 10, 34, 1]))
