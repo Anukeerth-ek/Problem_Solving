440f4ca5ac021dd3cca84c9a08ad77fac0093411
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-03-26 09:03:07 +0530

    Added new array problem

--- GeeksforGeeksProblem/Array/problem2.js
+++ GeeksforGeeksProblem/Array/problem2.js
@@ -1,4 +1,5 @@
-// You are given an array arr of size n - 1 that contains distinct integers in the range from 1 to n (inclusive). This array represents a permutation of the integers from 1 to n with one element missing. Your task is to identify and return the missing element.
+// You are given an array arr of size n - 1 that contains distinct integers in the range from 1 to n (inclusive). This array represents a
+// permutation of the integers from 1 to n with one element missing. Your task is to identify and return the missing element.
 
 // Examples:
 
@@ -6,6 +7,14 @@
 // Output: 4
 // Explanation: All the numbers from 1 to 5 are present except 4.
 
-const missingNumber = (arr) => {};
+const missingNumber = (arr) => {
+     const arrayLength = arr.length + 1;
 
-console.log(missingNumber([1, 2, 3, 5]));
+     const totalSum = (arrayLength * (arrayLength + 1)) / 2;
+
+     const totalArraySum = arr.reduce((acc, curr) => acc + curr, 0);
+
+     return totalSum - totalArraySum;
+};
+
+console.log(missingNumber([1, 2, 4, 5]));
