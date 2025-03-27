87ea32e498a774e389e2ff74ffef9c6d23ba799d
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-03-27 19:16:31 +0530

    Added new problem:

--- GeeksforGeeksProblem/Array/problem3.js
+++ GeeksforGeeksProblem/Array/problem3.js
@@ -1,4 +1,4 @@
-// Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then 
+// Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then
 // return -1.
 
 // Note: The second largest element should not be equal to the largest element.
@@ -10,12 +10,28 @@
 // Explanation: The largest element of the array is 35 and the second largest element is 34.
 
 class Solution {
-    // Function returns the second largest element
-    getSecondLargest(arr) {
-        
-    }
+     getSecondLargest(arr) {
+          if (arr.length < 2) return -1;
+
+          let largest = -Infinity,
+               secondLargest = -Infinity;
+
+          for (let num of arr) {
+               if (num > largest) {
+                    secondLargest = largest;
+                    console.log(secondLargest);
+                    largest = num;
+               } else if (num > secondLargest && num < largest) {
+                    secondLargest = num;
+               }
+          }
+
+          return secondLargest === -Infinity ? -1 : secondLargest;
+     }
 }
 
-const solution = new Solution()
+const solution = new Solution();
 
-console.log(solution.getSecondLargest([12, 35, 1, 10, 34, 1]))
+console.log(solution.getSecondLargest([12, 35, 1, 10, 34, 1]));
+// console.log(solution.getSecondLargest([10, 5, 10]));
+// console.log(solution.getSecondLargest([10, 10, 10]));
