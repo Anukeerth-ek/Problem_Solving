e2aa595ed882ed5a999ce283fdc5c2ce6251ec3a
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-04-03 08:57:41 +0530

    Added new geeks for geeks problem

--- GeeksforGeeksProblem/DSA/LogicBuildingProblems/problem1.js
+++ GeeksforGeeksProblem/DSA/LogicBuildingProblems/problem1.js
@@ -134,16 +134,38 @@
 
 // ans: 
 
-const handleDiceProblem = (number) => {
+// const handleDiceProblem = (number) => {
     //  FIRST APPROACH ______________________
     //  return 7 - number
     
     //  SECOND APPROACH ______________________
 
+// }
+
+// console.log(handleDiceProblem(3))
+// ______________________________________________________________________________________________________
+
+// ______________________________________________________________________________________________________
+
+// Nth term of AP from First Two Terms
+
+// Given two integers a1 and a2, the first and second terms of an Arithmetic Series respectively, the problem is to find the nth term of 
+// the series. 
+// Examples :
+
+// Input : a1 = 2,  a2 = 3,  n = 4
+// Output : 5
+// Explanation : The series is 2, 3, 4, 5, 6, ....   , thus the 4th term is 5. 
+
+// Input : a1 = 1, a2 = 3, n = 10
+// Output : 19
+// Explanation:  The series is: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21..... Thus,10th term is 19.
+
+// ans:
+
+function nthTermOfAP(number1, number2, nthNumber){
+    console.log(number1, number2, nthNumber)
 }
 
-console.log(handleDiceProblem(3))
+console.log(nthTermOfAP(2, 4, 5))
 // ______________________________________________________________________________________________________
-
-// ______________________________________________________________________________________________________
-
