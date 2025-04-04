c07cba2a0b2d4dd177f5c1201220a834a45ae683
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-04-04 11:26:36 +0530

    Added striver dsa problem

---
+++ DSA/Striver_DSA/PatternsProblems/StarPatterns/index.js
@@ -0,0 +1,14 @@
+
+// 1. Basic Start Pattern
+
+// const printBasicPattern = (limit)=> {
+//     for(let i = 0; i < limit; i++) {
+//         for(let j = 0; j < limit; j++) {
+//             process.stdout.write('*')
+//         }
+//         console.log()
+//     }
+// }   
+
+// printBasicPattern(10)
+// ________________________________________________________________
