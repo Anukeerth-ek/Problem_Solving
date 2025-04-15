70f6519120a71fb7315184bbb2b4f9cda39992dd
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-04-16 00:12:32 +0530

    Added new problem setup

--- DSA/Striver_DSA/PatternsProblems/StarPatterns/index.js
+++ DSA/Striver_DSA/PatternsProblems/StarPatterns/index.js
@@ -352,7 +352,7 @@
 // ____________________________________________
 
 // ____________________________________________
-// 18. 
+// 18.
 // const eighteenthProblem = (limit)=> {
 //     let k;
 //     for(let i = 0; i < limit; i++) {
@@ -366,3 +366,21 @@
 // }
 // eighteenthProblem(5)
 // ____________________________________________
+
+// ____________________________________________
+// 19.
+const nineteenthProblem = (limit) => {
+     let spaceController, stars;
+     for (let i = 0; i < limit; i++) {
+          for (let j = 1; j <= limit * 2; j++) {
+               if(i >= limit +2) {
+                spaceController = "-".repeat(i * 2);
+                stars = "*".repeat(limit - i);
+               }
+          }
+          console.log(`${stars}${spaceController}${stars}`);
+     }
+};
+nineteenthProblem(5);
+// ____________________________________________
+ 
