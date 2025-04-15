5cc35bcd5885302b570378ca6f9e049dcdc9c6b1
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-04-15 13:11:03 +0530

    formatted the file

--- DSA/Striver_DSA/PatternsProblems/StarPatterns/index.js
+++ DSA/Striver_DSA/PatternsProblems/StarPatterns/index.js
@@ -352,17 +352,17 @@
 // ____________________________________________
 
 // ____________________________________________
-18. 
-const eighteenthProblem = (limit)=> {
-    let k;
-    for(let i = 0; i < limit; i++) {
-        k = limit - i;
-        for(let j = 0; j <= i; j++) {
-            process.stdout.write(`${String.fromCharCode(64+k)} `)
-            k++;
-        }
-        console.log()
-    }
-}
-eighteenthProblem(5)
+// 18. 
+// const eighteenthProblem = (limit)=> {
+//     let k;
+//     for(let i = 0; i < limit; i++) {
+//         k = limit - i;
+//         for(let j = 0; j <= i; j++) {
+//             process.stdout.write(`${String.fromCharCode(64+k)} `)
+//             k++;
+//         }
+//         console.log()
+//     }
+// }
+// eighteenthProblem(5)
 // ____________________________________________
