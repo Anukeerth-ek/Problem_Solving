294a6d12d73e1efd256697cfcfe54b04c3a70f61
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-04-19 20:54:22 +0530

    Added new coding problem

--- DSA/Striver_DSA/BasicMath/index.js
+++ DSA/Striver_DSA/BasicMath/index.js
@@ -1,7 +1,7 @@
 // 1. Problem Statement: Given an integer N, return the number of digits in N.
 
-const countDigitNumber = (numbers)=> {
-  const digitCount = Math.floor(Math.log10(numbers)+1)
-  console.log(digitCount)
-}
-countDigitNumber(1234566)
+// const countDigitNumber = (numbers)=> {
+//   const digitCount = Math.floor(Math.log10(numbers)+1)
+//   console.log(digitCount)
+// }
+// countDigitNumber(1234566)
