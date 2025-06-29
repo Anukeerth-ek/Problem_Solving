ba3b936815900184afe0cfe4333ba061de891476
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-06-29 23:54:16 +0530

    fix: fixed indendation

--- DSA/Recursion/index.js
+++ DSA/Recursion/index.js
@@ -1,61 +1,61 @@
-// You are given an array of integers nums and an integer target.
+// // You are given an array of integers nums and an integer target.
 
-// Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to
-// target. Since the answer may be too large, return it modulo 109 + 7.
+// // Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to
+// // target. Since the answer may be too large, return it modulo 109 + 7.
 
-// Example 1:
+// // Example 1:
 
-// Input: nums = [3,5,6,7], target = 9
-// Output: 4
-// Explanation: There are 4 subsequences that satisfy the condition.
-// [3] -> Min value + max value <= target (3 + 3 <= 9)
-// [3,5] -> (3 + 5 <= 9)
-// [3,5,6] -> (3 + 6 <= 9)
-// [3,6] -> (3 + 6 <= 9)
-// Example 2:
+// // Input: nums = [3,5,6,7], target = 9
+// // Output: 4
+// // Explanation: There are 4 subsequences that satisfy the condition.
+// // [3] -> Min value + max value <= target (3 + 3 <= 9)
+// // [3,5] -> (3 + 5 <= 9)
+// // [3,5,6] -> (3 + 6 <= 9)
+// // [3,6] -> (3 + 6 <= 9)
+// // Example 2:
 
-// Input: nums = [3,3,6,8], target = 10
-// Output: 6
-// Explanation: There are 6 subsequences that satisfy the condition. (nums can have repeated numbers).
-// [3] , [3] , [3,3], [3,6] , [3,6] , [3,3,6]
-// Example 3:
+// // Input: nums = [3,3,6,8], target = 10
+// // Output: 6
+// // Explanation: There are 6 subsequences that satisfy the condition. (nums can have repeated numbers).
+// // [3] , [3] , [3,3], [3,6] , [3,6] , [3,3,6]
+// // Example 3:
 
-// Input: nums = [2,3,3,4,6,7], target = 12
-// Output: 61
-// Explanation: There are 63 non-empty subsequences, two of them do not satisfy the condition ([6,7], [7]).
-// Number of valid subsequences (63 - 2 = 61).
+// // Input: nums = [2,3,3,4,6,7], target = 12
+// // Output: 61
+// // Explanation: There are 63 non-empty subsequences, two of them do not satisfy the condition ([6,7], [7]).
+// // Number of valid subsequences (63 - 2 = 61).
 
-// Constraints:
+// // Constraints:
 
-// 1 <= nums.length <= 105
-// 1 <= nums[i] <= 106
-// 1 <= target <= 106
+// // 1 <= nums.length <= 105
+// // 1 <= nums[i] <= 106
+// // 1 <= target <= 106
 
-const handleRecusion = (arr, target) => {
-     const MOD = 1e9 + 7;
-     nums.sort((a, b) => a - b);
+// const handleRecusion = (arr, target) => {
+//      const MOD = 1e9 + 7;
+//      nums.sort((a, b) => a - b);
 
-     const n = nums.length;
-     const pow = new Array(n).fill(1);
-     // Precompute powers of 2 modulo MOD
-     for (let i = 1; i < n; i++) {
-          pow[i] = (pow[i - 1] * 2) % MOD;
-     }
+//      const n = nums.length;
+//      const pow = new Array(n).fill(1);
+//      // Precompute powers of 2 modulo MOD
+//      for (let i = 1; i < n; i++) {
+//           pow[i] = (pow[i - 1] * 2) % MOD;
+//      }
 
-     let left = 0,
-          right = n - 1;
-     let result = 0;
+//      let left = 0,
+//           right = n - 1;
+//      let result = 0;
 
-     while (left <= right) {
-          if (nums[left] + nums[right] <= target) {
-               result = (result + pow[right - left]) % MOD;
-               left++;
-          } else {
-               right--;
-          }
-     }
+//      while (left <= right) {
+//           if (nums[left] + nums[right] <= target) {
+//                result = (result + pow[right - left]) % MOD;
+//                left++;
+//           } else {
+//                right--;
+//           }
+//      }
 
-     return result;
-};
+//      return result;
+// };
 
-console.log(handleRecusion([3, 5, 6, 7], 9));
+// console.log(handleRecusion([3, 5, 6, 7], 9));
