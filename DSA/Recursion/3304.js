fb28b2a227357a8593add58603b7201ed50293f2
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-07-03 17:15:41 +0530

    feat: added new problem

--- DSA/Recursion/3304.js
+++ DSA/Recursion/3304.js
@@ -1,40 +1,40 @@
-Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+// Alice and Bob are playing a game. Initially, Alice has a string word = "a".
 
-You are given a positive integer k.
+// You are given a positive integer k.
 
-Now Bob will ask Alice to perform the following operation forever:
+// Now Bob will ask Alice to perform the following operation forever:
 
-Generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original 
-word.
-For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
+// Generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original 
+// word.
+// For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
 
-Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
+// Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
 
-Note that the character 'z' can be changed to 'a' in the operation.
+// Note that the character 'z' can be changed to 'a' in the operation.
 
 
-Example 1:
+// Example 1:
 
-Input: k = 5
+// Input: k = 5
 
-Output: "b"
+// Output: "b"
 
-Explanation:
+// Explanation:
 
-Initially, word = "a". We need to do the operation three times:
+// Initially, word = "a". We need to do the operation three times:
 
-Generated string is "b", word becomes "ab".
-Generated string is "bc", word becomes "abbc".
-Generated string is "bccd", word becomes "abbcbccd".
-Example 2:
+// Generated string is "b", word becomes "ab".
+// Generated string is "bc", word becomes "abbc".
+// Generated string is "bccd", word becomes "abbcbccd".
+// Example 2:
 
-Input: k = 10
+// Input: k = 10
 
-Output: "c"
+// Output: "c"
 
  
 
-Constraints:
+// Constraints:
 
-1 <= k <= 500
+// 1 <= k <= 500
 
