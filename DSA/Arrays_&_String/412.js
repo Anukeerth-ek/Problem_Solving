5a1625d002a07b691cc75e1474657896aa98d83d
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-07-07 20:42:20 +0530

    feat: added optimised solution

--- DSA/Arrays_&_String/412.js
+++ DSA/Arrays_&_String/412.js
@@ -28,7 +28,7 @@
 
 
 const handleFizzBuzz = (number)=> {
-    let result = [].fill(number);
+    let result = [];
 
     for(let i = 1; i <= number; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
