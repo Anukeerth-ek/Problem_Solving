4581af61a092f107ab4fbbc4d8bc1ddd7db6df6b
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-07-20 01:28:04 +0530

    feat: added basic problem verion

--- DSA/LeetCodeWeekly/3618.js
+++ DSA/LeetCodeWeekly/3618.js
@@ -10,8 +10,6 @@
 
 // Note: An empty array has a sum of 0.
 
- 
-
 // Example 1:
 
 // Input: nums = [2,3,4]
@@ -36,9 +34,30 @@
 // The remaining elements, nums[0] = -1 and nums[1] = 5 are placed in array B.
 // sum(A) = 7 + 0 = 7, sum(B) = -1 + 5 = 4.
 // The absolute difference is |7 - 4| = 3.
- 
 
 // Constraints:
 
 // 1 <= nums.length <= 105
 // -109 <= nums[i] <= 109
+
+var splitArray = function (nums) {
+     const primeNumber = [2, 3, 5, 7, 11, 13, 17, 19];
+
+     let arrayA = [];
+     let arrayB = [];
+
+     for (let num = 0; num < nums.length; num++) {
+          if (primeNumber.includes(num)) {
+               arrayA.push(nums[num]);
+          } else {
+               arrayB.push(nums[num]);
+          }
+     }
+     const sumOfA = arrayA.reduce((acc, red) => acc + red, 0);
+     const sumOfB = arrayB.reduce((acc, red) => acc + red, 0);
+
+     const result = sumOfA - sumOfB
+     return Math.abs(result);
+};
+
+console.log(splitArray([2, 3, 4]));
