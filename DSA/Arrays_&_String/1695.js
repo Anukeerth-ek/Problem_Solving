7b0d7e01bf3538f44cc24a84f4f1fad81a779f1d
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-07-24 23:31:53 +0530

    feat: added new proble solution

--- DSA/Arrays_&_String/1695.js
+++ DSA/Arrays_&_String/1695.js
@@ -27,24 +27,24 @@
 
 // ans:
 
-var maximumUniqueSubarray = function(nums) {
-let seen = new Set();
-    let left = 0, maxSum = 0, currentSum = 0;
+// var maximumUniqueSubarray = function(nums) {
+// let seen = new Set();
+//     let left = 0, maxSum = 0, currentSum = 0;
 
-    for (let right = 0; right < nums.length; right++) {
-        while (seen.has(nums[right])) {
-            seen.delete(nums[left]);
-            currentSum -= nums[left];
-            left++;
-        }
+//     for (let right = 0; right < nums.length; right++) {
+//         while (seen.has(nums[right])) {
+//             seen.delete(nums[left]);
+//             currentSum -= nums[left];
+//             left++;
+//         }
 
-        seen.add(nums[right]);
-        currentSum += nums[right];
-        maxSum = Math.max(maxSum, currentSum);
-    }
+//         seen.add(nums[right]);
+//         currentSum += nums[right];
+//         maxSum = Math.max(maxSum, currentSum);
+//     }
 
-    return maxSum;
-};
+//     return maxSum;
+// };
 
-console.log(maximumUniqueSubarray([187,470,25,436,538,809,441,167,477,110,275,133,666,345,411,459,490,266,987,965,429,166,809,340,467,318,125,165,809,610,31,585,970,306,42,189,169,743,78,810,70,382,367,490,787,670,476,278,775,673,299,19,893,817,971,458,409,886,434]))
-console.log(maximumUniqueSubarray([5,2,1,2,5,2,1,2,5]))
+// console.log(maximumUniqueSubarray([187,470,25,436,538,809,441,167,477,110,275,133,666,345,411,459,490,266,987,965,429,166,809,340,467,318,125,165,809,610,31,585,970,306,42,189,169,743,78,810,70,382,367,490,787,670,476,278,775,673,299,19,893,817,971,458,409,886,434]))
+// console.log(maximumUniqueSubarray([5,2,1,2,5,2,1,2,5]))
