901eb9f047de343fbaae408c822b0c2bbfbf1163
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-07-25 20:35:34 +0530

    feat: added new way to solve

--- Basic/problems/palindrome.js
+++ Basic/problems/palindrome.js
@@ -3,9 +3,8 @@
 // 1. 121, 1331, 12321, 1001, 22541, 1551
 
 const handlePalindrome = (number) => {
-
-    
-
+     let resultValue = String(number).split("").reverse().join("");
+     return Number(resultValue) === number ? true : false;
 };
 
 console.log(handlePalindrome(22541));
