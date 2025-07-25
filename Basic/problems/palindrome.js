3ba58da703b5bea68cf7e9fa43c41ddf85595750
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-07-25 20:22:34 +0530

    feat: added new coding problem

---
+++ Basic/problems/palindrome.js
@@ -0,0 +1,13 @@
+// Check for palindrome
+
+// 1. 121, 1331, 12321, 1001, 22541, 1551
+
+const handlePalindrome = (number) => {
+
+    
+
+};
+
+console.log(handlePalindrome(22541));
+console.log(handlePalindrome(12321));
+console.log(handlePalindrome(121));
