51466537cfc414ed610dec541472b960000aea7d
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-07-26 11:43:28 +0530

    feat(indendation): added indendation

--- Basic/problems/palindrome.js
+++ Basic/problems/palindrome.js
@@ -2,11 +2,11 @@
 
 // 1. 121, 1331, 12321, 1001, 22541, 1551
 
-const handlePalindrome = (number) => {
-     let resultValue = String(number).split("").reverse().join("");
-     return Number(resultValue) === number ? true : false;
-};
+// const handlePalindrome = (number) => {
+//      let resultValue = String(number).split("").reverse().join("");
+//      return Number(resultValue) === number ? true : false;
+// };
 
-console.log(handlePalindrome(22541));
-console.log(handlePalindrome(12321));
-console.log(handlePalindrome(121));
+// console.log(handlePalindrome(22541));
+// console.log(handlePalindrome(12321));
+// console.log(handlePalindrome(121));
