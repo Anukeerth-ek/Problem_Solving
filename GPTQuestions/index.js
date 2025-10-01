cb5e7fbbabe352e4233e55ba35bfafb17651aaf1
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-10-01 20:43:58 +0530

    feat: added coding problem

---
+++ GPTQuestions/index.js
@@ -0,0 +1,30 @@
+// Input: "hello"
+// Output: "olleh"
+// function reverseString(str) {
+//      return [...str].reverse().join("")
+// }
+
+// console.log(reverseString("hello"));
+// _____________________________________________________
+
+// Input: [1,2,3,2,4,1]
+// Output: [1,2]
+
+// function findDuplicates(arr) {
+//      const seen = new Set()
+
+//      const duplicate = new Set()
+
+//      for (let num of arr) {
+//           if (seen.has(num)) {
+//                duplicate.add(num)
+//           }
+//           seen.add(num)
+//      }
+//      return [...duplicate]
+// }
+
+// console.log(findDuplicates([1, 2, 3, 2, 4, 1]))
+// _____________________________________________________
+
+
