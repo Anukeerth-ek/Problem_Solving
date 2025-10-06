07b99c8196acf2d7c3000f8864041b828ad37243
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-10-06 08:20:00 +0530

    feat: added new coding problem

--- GPTQuestions/index.js
+++ GPTQuestions/index.js
@@ -1,4 +1,5 @@
-// Input: "hello"
+// _____________________________________________________
+//1.  Input: "hello"
 // Output: "olleh"
 // function reverseString(str) {
 //      return [...str].reverse().join("")
@@ -7,7 +8,7 @@
 // console.log(reverseString("hello"));
 // _____________________________________________________
 
-// Input: [1,2,3,2,4,1]
+//2.  Input: [1,2,3,2,4,1]
 // Output: [1,2]
 
 // function findDuplicates(arr) {
@@ -27,4 +28,58 @@
 // console.log(findDuplicates([1, 2, 3, 2, 4, 1]))
 // _____________________________________________________
 
+// _____________________________________________________
 
+// 3. Find the largest/smallest element in an array
+
+// const findSmallest = (arr) => {
+
+// Lets find smallest element here
+
+// if (!Array.isArray(arr) || arr.length <= 0) {
+//     return "Please provide an array values"
+// }
+
+// let smallestNumber = arr[0]
+
+// for (let i = 1; i < arr.length; i++) {
+//     if (smallestNumber > arr[i]) {
+//         smallestNumber = arr[i]
+//     }
+// }
+// return smallestNumber;
+
+// _______________________________________________
+// Lets find smallest element here 2nd step
+
+// const smallestElement = arr.reduce((curr, acc) => curr < acc ? curr : acc)
+// console.log(smallestElement)
+// }
+
+// console.log(findSmallest([2, 5, 8, 10, 24, 1]))
+// _____________________________________________________
+
+// _____________________________________________________
+// 4. Remove duplicates from an array
+
+// const removeDuplicates = (arr) => {
+//     // lets find duplicates here
+    
+//     if (!Array.isArray(arr) || arr.length <= 0) {
+//         return "Please enter a valid array"
+//     }
+
+//      const seen = new Set();
+//      let newArr = [];
+
+    
+//     for (let element = 0; element < arr.length; element++) {
+//         if (!seen.has(arr[element])) {
+//             seen.add(arr[element]) 
+//             newArr.push(arr[element])
+//         }
+//     }
+//     return newArr
+// };
+// console.log(removeDuplicates([1, 2, 5, 8, 10, 24, 8, 1]));
+// _____________________________________________________
