bb6ed286dc6480b848c9fef84d3cf6e1eb651415
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-11-28 21:40:09 +0530

    Feat(Problem): added new coding problem

---
+++ Interview_Problems/question2.js
@@ -0,0 +1,21 @@
+// 2️⃣ Find Missing Number
+// Given nums = [0..n] missing one number → return missing one.
+
+// const findMissingNumber = (number) => {
+    
+//     const length = number.length;
+
+//     const expectedSum = length * (length + 1) / 2;
+
+//     let sum = 0;
+
+//     for (let num of number) {
+//         sum += num;
+//     }
+
+//     return expectedSum - sum;
+
+// }
+
+// console.log(findMissingNumber([3, 0, 1]))
+// console.log(findMissingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
