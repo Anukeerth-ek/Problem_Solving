d057463066f2281b31b984131d80661089333c49
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-12-23 10:31:56 +0530

    Feat: added new problem

--- DSA/Striver_DSA/BasicMath/index.js
+++ DSA/Striver_DSA/BasicMath/index.js
@@ -307,7 +307,7 @@
 
 // ans:
 // const handleStringPalindrome = (str) => {
-    
+
 //     const checkPalindrome = (s, start, end)=> {
 
 //         if(start > end) return true;
@@ -323,7 +323,7 @@
 //  ______________________
 
 //  ______________________
-// 8. Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 
+// 8. Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each
 // 0 <= i < nums.length and return it.
 
 // A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
@@ -332,7 +332,7 @@
 
 // Input: nums = [0,2,1,5,3,4]
 // Output: [0,1,2,4,5,3]
-// Explanation: The array ans is built as follows: 
+// Explanation: The array ans is built as follows:
 // ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
 //     = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
 //     = [0,1,2,4,5,3]
@@ -344,17 +344,36 @@
 // ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
 //     = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
 //     = [4,5,0,1,2,3]
- 
 
 // Constraints:
 
 // 1 <= nums.length <= 1000
 // 0 <= nums[i] < nums.length
 // The elements in nums are distinct.
- 
 
 // Follow-up: Can you solve it without using an extra space (i.e., O(1) memory)?
 
 // ans:
 
 //  ______________________
+
+// 9.Print Fibonacci Series up to Nth term
+
+const printFibonacciSeries = (number) => {
+
+ return inner(number)
+};
+
+    function inner(n) {
+        if (n <= 1) {
+            return n;
+        }
+        
+          let firstNumber = inner(n - 1);
+          let secondNumber = inner( n - 2);
+
+          return firstNumber + secondNumber;
+}
+     
+
+console.log(printFibonacciSeries(5));
