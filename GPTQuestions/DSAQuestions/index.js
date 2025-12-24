b3ccd7e26225e6ca938e8b53c9a1152de8ab3651
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-12-24 19:58:07 +0530

    feat: added new question

--- GPTQuestions/DSAQuestions/index.js
+++ GPTQuestions/DSAQuestions/index.js
@@ -1,11 +1,14 @@
-const printNumber = (number) => {
-    if (number === 0) {
-        return
-    }
+// const printNumber = (number) => {
+//     if (number === 0) {
+//         return
+//     }
 
-    console.log(number)
+//     console.log(number)
 
-    printNumber(number - 1)
-};
+//     printNumber(number - 1)
+// };
 
-console.log(printNumber(3));
+// console.log(printNumber(3));
+
+// 2. Reconstruct input array from output averages
+
