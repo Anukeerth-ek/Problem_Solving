9daf8773f996b979d8e0ed5e5735c7a0a8d7e3d6
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-12-29 10:15:01 +0530

    feat: added new js problem

--- GPTQuestions/DSAQuestions/index.js
+++ GPTQuestions/DSAQuestions/index.js
@@ -63,3 +63,32 @@
 // };
 
 // console.log(findMissingNumbers([2, 4, 6, 9]));
+
+// _________________________________________________
+
+// 5. Remove Duplicates from Array
+
+// const findDuplicates = (array) => {
+    
+//     if (!Array.isArray(array)) {
+//         throw new TypeError("Expecting Array")
+//     }
+
+//     if (array.length === 0) return [];
+
+//     const set = new Set();
+
+//     let result = [];
+
+//     for (let i = 0; i < array.length; i++) {
+//         if (!set.has(array[i])) {
+//             set.add(array[i]);
+
+//             result.push(array[i])
+//         }
+//     }
+//     return result;
+// }
+
+// console.log(findDuplicates([1,2,2,3,4,4,5]))
+// _________________________________________________
