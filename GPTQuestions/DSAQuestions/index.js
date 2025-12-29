756c3f5d6c3faa081529f4a4283e57594a44eb80
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-12-29 10:00:36 +0530

    Feat: added new coding problem

--- GPTQuestions/DSAQuestions/index.js
+++ GPTQuestions/DSAQuestions/index.js
@@ -16,18 +16,16 @@
 // _________________________________________________
 // 3. Find Duplicates Element in the array
 
-
 // const findDuplicates = (arr) => {
 //     if (arr.length === 0) return false;
 
 //     let map = new Map();
-    
-     
+
 //     for (let i = 0; i < arr.length; i++) {
 //         if (map.has(arr[i])) {
-            
+
 //             return true
-            
+
 //         }
 //         else {
 //             map.set(arr[i], true)
@@ -39,3 +37,29 @@
 // console.log(findDuplicates([1, 2, 1, 3, 4]))
 // console.log(findDuplicates([3, 4, 5, 7, 10]))
 // _________________________________________________
+
+// 4. Find Missing Numbers in a Range
+
+// const findMissingNumbers = (arr) => {
+
+//     if (!Array.isArray(arr)) {
+//         throw new TypeError("Expecting Array");
+//     }
+
+//     if (arr.length === 0) return [];
+
+//     const LargestNumber = Math.max(...arr);
+
+//     const set = new Set(arr);
+
+//     let missingNumbers = [];
+
+//     for (let i = 1; i < LargestNumber; i++){
+//         if (!set.has(i)) {
+//             missingNumbers.push(i)
+//         }
+//     }
+//     return missingNumbers;
+// };
+
+// console.log(findMissingNumbers([2, 4, 6, 9]));
